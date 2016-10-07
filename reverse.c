6acf9a725a53af4be11ab26153d5c6f5781fe4a3
Suneel Dogra <kumar_sunil@live.com>
2016-10-07 10:20:07 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,12 @@
+//WAP  to Revere the String
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char str[20];
+printf("Enter the String");
+gets(str);
+printf("The reverse of String is %s",strrev(str));
+return 0;
+}
